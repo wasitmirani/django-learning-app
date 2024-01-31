6c3efc3fd3c51864f8d99b9180db77e8a407c859
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2024-02-01 01:33:57 +0500

    add: layout config

--- home/views.py
+++ home/views.py
@@ -5,4 +5,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, this is the home view.")
+    data=[
+        {
+            "name": "John",
+            "age": 30
+        },
+        {
+            "name": "Mary",
+            "age": 25
+        },
+        {
+            "name": "Peter",
+            "age": 20
+        },
+        {
+            "name": "Lisa",
+            "age": 25
+        },
+    ]
+    return render(request, 'pages/index.html', context={'data': data})
+
+
+def about(request):
+    return render(request, 'pages/about.html')
